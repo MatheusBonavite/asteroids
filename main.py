01f4c5ee452f161ebcd03e8a7a787ff67f50cac5
MatheusBonavite <matheusbonavite@gmail.com>
2024-12-31 14:52:59 -0300

    Add constants file to hold game magic numbers

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pygame
+import constants
 
+# The simplest game loop has 3 steps:
+# 1 - Check for player inputs
+# 2 - Update the game world
+# 3 - Draw the game to the screen
+# --- Remember: to create a good user experience,
+# ---           such steps should happen many times per second
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
